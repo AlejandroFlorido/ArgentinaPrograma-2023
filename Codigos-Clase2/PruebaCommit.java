75358ea3c849e5e898d0134d5800896558d33811
Alejandro <floridosalejandro@gmail.com>
2023-02-27 19:55:01 -0300

    Prueba commit

---
+++ Codigos-Clase2/PruebaCommit.java
@@ -0,0 +1,8 @@
+class PruebaCommit
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println ("Este es un algoritmo que me va a ayudar a comprobar que este haciendo los pasos del push correctamente");
+    }
+}
