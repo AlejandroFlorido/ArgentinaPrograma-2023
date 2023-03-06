0de815b2c04e3e721e66e985124203ead101467b
Alejandro <floridosalejandro@gmail.com>
2023-03-05 21:55:14 -0300

    Solucione el ejercicio condicional

--- Condicional_IF.java
+++ Condicional_IF.java
@@ -5,24 +5,22 @@ class CondicionalIF
    
     public static void main(String args[])
     {
-
         Scanner V= new Scanner(System.in);
             System.out.println("Ingrese el primer numero");
             int Num1= V.nextInt();
-
             System.out.println("Ingrese el segundo numero");
             int Num2= V.nextInt();
 
-                if (Num1 > Num2) {
-                    System.out.println("El numero "+Num1+" es mas grande que el "+Num2);
+            if (Num1 > Num2) {
+                System.out.println("El numero "+Num1+"Es mayor al numero "+Num2);
+            }
+            else {
+                if (Num1 < Num2) {
+                    System.out.println("El numero "+Num2+"es mayor que el numero "+Num1);
                 }
                 else {
-                    if (Num1 < Num2)
-                        System.out.println("El numero "+Num2+" es mas grande que el "+Num1);
-                    }
-                        {
-                            if (Num1 == Num2) System.out.println("Ambos son iguales");
-                        }
-            V.close();
+                    System.out.println("Ambos son iguales");
+                }
+            }
     }
-}
+}    
